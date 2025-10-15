9209d454b564173220a0cf21ceaf97458f488a8f
Kaue Moreira <kauemoreiraofc2@gmail.com>
2025-10-15 15:05:05 +0000

    MUDE TA FEIO NAO TA IGUAL UM REMEDIO

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -11,16 +11,16 @@ export function Logo({ className }: { className?: string }) {
             xmlns="http://www.w3.org/2000/svg"
             className="h-8 w-auto"
         >
-            <g transform="rotate(45 50 50)">
-                <path
-                    d="M35,15 C20,15,20,35,35,35 L65,35 C80,35,80,15,65,15 Z"
-                    fill="hsl(var(--primary))"
-                />
-                <path
-                    d="M65,85 C80,85,80,65,65,65 L35,65 C20,65,20,85,35,85 Z"
-                    fill="hsl(var(--secondary))"
-                />
-            </g>
+          <g transform="rotate(45 50 50)">
+            <path
+              d="M50,15 a35,17.5 0 0,0 0,70"
+              fill="hsl(var(--secondary))"
+            />
+            <path
+              d="M50,15 a35,17.5 0 0,1 0,70"
+              fill="hsl(var(--primary))"
+            />
+          </g>
         </svg>
     </div>
   );
