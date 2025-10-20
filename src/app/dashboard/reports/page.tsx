e32ea21a9493d85096132a68d6b2abd34b86b6c5
Kaue Moreira <kauemoreiraofc2@gmail.com>
2025-10-20 18:36:54 +0000

    15:33:45.190 Running build in Portland, USA (West) – pdx1 15:33:45.191 B

--- src/app/dashboard/reports/page.tsx
+++ src/app/dashboard/reports/page.tsx
@@ -1,9 +1,12 @@
 
+'use server';
 
 import { Suspense } from 'react';
 import { getProducts, getAllPatients, getAllDispensations, getUnits, getOrders, getStockMovements } from "@/lib/data";
 import { Skeleton } from "@/components/ui/skeleton";
 import { ReportsClientPage } from "./client-page";
+
+export const runtime = 'edge'; // Força a execução no Vercel Edge Runtime para otimizar o tamanho do pacote.
 
 async function ReportsData() {
     const [products, patients, dispensations, units, orders, stockMovements] = await Promise.all([
