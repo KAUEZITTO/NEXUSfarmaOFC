7cdee9a5a3010a056b3d84b6e1bb1751b3bf5348
Kaue Moreira <kauemoreiraofc2@gmail.com>
2025-10-20 10:55:32 +0000

    npm i @vercel/speed-insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import './globals.css';
 import { cn } from '@/lib/utils';
 import { Inter } from 'next/font/google';
 import AuthProvider from '@/components/auth/auth-provider';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({
   subsets: ['latin'],
@@ -34,6 +35,7 @@ export default function RootLayout({
           {children}
         </AuthProvider>
         <Toaster />
+        <SpeedInsights />
       </body>
     </html>
   );
